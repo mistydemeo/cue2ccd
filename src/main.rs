78a389b99337a5255c6dfffa7b67a7f054376895
Misty De Meo <mistydemeo@gmail.com>
2023-06-19 15:09:47 -0700

    Move subchannel generation to cdrom crate

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,7 @@ use std::io::{self, Write};
 use std::path::Path;
 use std::process::exit;
 
-use cdrom::{Disc, TrackMode};
-use cdrom_crc::{crc16, CRC16_INITIAL_CRC};
+use cdrom::Disc;
 use cue::cd::CD;
 use cue::track::Track;
 
@@ -21,100 +20,9 @@ fn has_multiple_files(tracks: Vec<Track>) -> bool {
     false
 }
 
-// The subchannel data contains extra sidecar metadata required to read
-// the disc, but which isn't a part of the data itself.
-// Some applications can read CloneCD data with zeroed out subchannel data
-// provided that the more verbose form of the CCD control file is used,
-// but other applications require real subchannel data no matter what.
-//
-// The CloneCD format stores the subchannel data in a sidecar file, which
-// is essentially identical to the data on the disc with a few exceptions:
-// 1) The leadin (the first 150 sectors) is omitted, and so is the
-//    table of contents that's usually stored there.
-// 2) The first two subchannel bytes, which contain sync words, are omitted.
-// 3) The subchannel data is unrolled into eight sequential sections of
-//    12 bytes instead of interleaved bits. This is easier to read and write
-//    in non-streaming applications.
-// In total, we need to write 96 bytes of subchannel data for each sector.
-//
-// More information is in ECMA-130:
-// http://www.ecma-international.org/publications/standards/Ecma-130.htm
-
 // TODO handle incorrect sector sizes and remainders
 fn sector_count(size: u64, sector_size: u64) -> u64 {
     size / sector_size
-}
-
-fn bcd(dec: i64) -> i64 {
-    ((dec / 10) << 4) | (dec % 10)
-}
-
-fn generate_q_subchannel(
-    absolute_sector: i64,
-    relative_sector: i64,
-    track: usize,
-    index: usize,
-    track_type: TrackMode,
-) -> Vec<u8> {
-    // This channel made up of a sequence of bits; we'll start by
-    // zeroing it out, then setting individual bits.
-    let mut q = vec![0; 12];
-
-    // First four bits are the control field.
-    // We only care about setting the data bit, 1; the others are
-    // irrelevant for this application.
-    match track_type {
-        TrackMode::Audio => (),
-        _ => q[0] |= 1 << 6,
-    };
-
-    // Next four bits indicate the mode of the Q channel.
-    // There are three modes:
-    // * 1 - Table of contents (used during the lead-in)
-    // * 2 - Media Catalog Number
-    // * 3 - International Standard Recording Code (ISRC)
-    // In practice, we're always generating mode 1
-    // every sector so we'll hardcode this.
-    q[0] |= 1 << 0;
-    // OK, it's data time! This is the next 9 bytes.
-    // This contains timing info for the current track.
-    q[1] = track as u8;
-
-    // Next is the index. While it supports values up to 99,
-    // usually only two values are seen:
-    // 00 - Pregap or postgap
-    // 01 - First index within the track, or leadout
-    q[2] = index as u8;
-
-    // The next three fields, MIN, SEC, and FRAC, are the
-    // running time within each index.
-    // FRAC is a unit of 1/75th of a second, e.g. the
-    // duration of exactly one sector.
-    // In the pregap, this starts at the pregap duration
-    // and counts down to 0.
-    // In the actual content, this starts at 0 and
-    // counts up.
-    // MIN
-    q[3] = bcd(relative_sector / 4500) as u8;
-    // SEC
-    q[4] = bcd((relative_sector / 75) % 60) as u8;
-    // FRAC
-    q[5] = bcd(relative_sector % 75) as u8;
-    // Next byte is always zero
-    q[6] = 0;
-    // The next three bytes provide an absolute timestamp,
-    // rather than a timestamp within the current track.
-    // These three fields, A-MIN, A-SEC, and A-FRAC, are
-    // stored the same way as the relative timestamps.
-    q[7] = bcd(absolute_sector / 4500) as u8;
-    q[8] = bcd((absolute_sector / 75) % 60) as u8;
-    q[9] = bcd(absolute_sector % 75) as u8;
-    // The last two bytes contain a CRC of the main data.
-    let crc = crc16(&q[0..10], CRC16_INITIAL_CRC);
-    q[10] = ((crc >> 8) & 0xFF) as u8;
-    q[11] = (crc & 0xFF) as u8;
-
-    q
 }
 
 fn main() -> io::Result<()> {
@@ -157,28 +65,7 @@ fn main() -> io::Result<()> {
 
     let disc = Disc::from_cuesheet(cd, sectors as i64);
     for sector in disc.sectors() {
-        // The first sector of the disc, and only the first sector,
-        // gets an FFed out P sector like a pregap. Every other non-pregap
-        // sector uses 0s.
-        // For players which ignore the Q subchannel, this allows
-        // locating the start of tracks.
-        let p: Vec<u8> = if sector.start == 0 || sector.index.number == 0 {
-            vec![0xFF; 12]
-        } else {
-            vec![0; 12]
-        };
-        let q = generate_q_subchannel(
-            sector.absolute_start,
-            sector.relative_position,
-            sector.track.number,
-            sector.index.number,
-            sector.track.mode,
-        );
-        assert_eq!(12, q.len());
-        let rest: Vec<u8> = vec![0; 72];
-        sub_write.write_all(&p)?;
-        sub_write.write_all(&q)?;
-        sub_write.write_all(&rest)?;
+        sub_write.write_all(&sector.generate_subchannel())?;
     }
 
     Ok(())
