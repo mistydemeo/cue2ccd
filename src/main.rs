b4759b2c50afce77953b4784aa876cac82b55882
Misty De Méo <misty@axo.dev>
2023-08-03 16:20:53 -0700

    Handle more invalid file cases
    * Cuesheets containing non-raw audio (WAV)
    * Cuesheets contianing non-raw data (ISO files, etc)

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use std::path::Path;
 use cdrom::Disc;
 use clap::Parser;
 use cue::cd::CD;
-use cue::track::Track;
+use cue::track::{Track, TrackMode};
 use miette::{Diagnostic, Result};
 use thiserror::Error;
 
@@ -18,6 +18,14 @@ enum Cue2CCDError {
     #[error("A data file specified in the cuesheet is missing.")]
     #[diagnostic(help("Missing file: {}", missing_file.display()))]
     MissingFileError { missing_file: std::path::PathBuf },
+
+    #[error("This tool only supports raw disc images")]
+    #[diagnostic(help("cuesheets containing .wav files are not compatible."))]
+    WaveFile {},
+
+    #[error("This tool only supports raw disc images")]
+    #[diagnostic(help("cuesheets containing ISOs or other non-raw data are not compatible."))]
+    CookedData {},
 
     #[error(transparent)]
     IO(#[from] std::io::Error),
@@ -37,7 +45,7 @@ struct Args {
     filename: String,
 }
 
-fn has_multiple_files(tracks: Vec<Track>) -> bool {
+fn has_multiple_files(tracks: &[Track]) -> bool {
     let mut tracks_iter = tracks.iter();
     let base_file = tracks_iter.next().unwrap().get_filename();
     for track in tracks_iter {
@@ -47,6 +55,21 @@ fn has_multiple_files(tracks: Vec<Track>) -> bool {
     }
 
     false
+}
+
+fn validate_mode(tracks: &[Track]) -> Result<(), Cue2CCDError> {
+    for track in tracks {
+        if track.get_filename().ends_with(".wav") {
+            return Err(Cue2CCDError::WaveFile {});
+        }
+        match track.get_mode() {
+            TrackMode::Mode1 | TrackMode::Mode2 | TrackMode::Mode2Form1 | TrackMode::Mode2Form2 => {
+                return Err(Cue2CCDError::CookedData {});
+            }
+            _ => (),
+        }
+    }
+    Ok(())
 }
 
 // TODO handle incorrect sector sizes and remainders
@@ -68,16 +91,17 @@ fn work() -> Result<(), Cue2CCDError> {
     let cd = CD::parse(cue_sheet)?;
 
     let tracks = cd.tracks();
-    if has_multiple_files(tracks) {
+    if has_multiple_files(&tracks) {
         return Err(Cue2CCDError::MultipleFilesError {})?;
     }
+    validate_mode(&tracks)?;
+
     let fname = cd.tracks().first().unwrap().get_filename();
     let file = root.join(fname);
     if !file.is_file() {
         return Err(Cue2CCDError::MissingFileError { missing_file: file })?;
     }
     let filesize = file.metadata()?.len();
-    // TODO deal with non-2352 byte per sector images (treat as an error?)
     let sectors = sector_count(filesize, 2352);
     println!("Image is {} sectors long", sectors);
 
